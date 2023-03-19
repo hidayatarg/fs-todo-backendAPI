dd70d8b25089cab2c94f11fcc91967b8ab05a305
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-03-19 14:17:32 +0300

    Added Cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
+
+// please disable the following line if you don't want to use cors
+app.use(cors())
 
 // to read the body of the request
 app.use(express.json())
